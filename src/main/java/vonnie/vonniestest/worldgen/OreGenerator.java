a5d6e1b98dfebe0284b60d396f6624bd1962a3ff
Jkosio23 <jkosio23@gmail.com>
2018-12-19 22:10:51 -0500

    Partway Through adding ore gen

--- src/main/java/vonnie/vonniestest/worldgen/OreGenerator.java
+++ src/main/java/vonnie/vonniestest/worldgen/OreGenerator.java
@@ -1,4 +1,18 @@
 package vonnie.vonniestest.worldgen;
 
-public class OreGenerator {
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+import java.util.Random;
+
+public class OreGenerator implements IWorldGenerator {
+    public static final String RETRO_NAME = "vonniestestOreGen";
+    public static OreGenerator instance = new OreGenerator();
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+
+    }
 }
