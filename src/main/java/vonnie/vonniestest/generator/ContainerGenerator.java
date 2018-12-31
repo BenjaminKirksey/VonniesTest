0613b2bbf6e480e056489df6ff6b248a5cbf027e
Jkosio23 <jkosio23@gmail.com>
2018-12-31 16:59:57 -0500

    Fixed slot Misalignment and Implemented Power usage

--- src/main/java/vonnie/vonniestest/generator/ContainerGenerator.java
+++ src/main/java/vonnie/vonniestest/generator/ContainerGenerator.java
@@ -1,9 +1,13 @@
 package vonnie.vonniestest.generator;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IContainerListener;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import vonnie.vonniestest.network.Messages;
+import vonnie.vonniestest.network.PacketSyncMachineState;
 import vonnie.vonniestest.tools.IMachineStateContainer;
 
 public class ContainerGenerator extends Container implements IMachineStateContainer {
@@ -19,14 +23,14 @@ public class ContainerGenerator extends Container implements IMachineStateContai
         // Slots for the main inventory
         for (int row = 0; row < 3; ++row) {
             for (int col = 0; col <9; ++col) {
-                int x = 10 + col * 18;
+                int x = 8 + col * 18;
                 int y = row * 18 + 70;
-                this.addSlotToContainer(new Slot(playerInventory, col + row * 9 + 10, x, y));
+                this.addSlotToContainer(new Slot(playerInventory, col + row * 9 + 9, x, y));
             }
         }
         // Slots for the hotbar
         for (int row = 0; row < 9; ++row) {
-            int x = 10 + row * 18;
+            int x = 8 + row * 18;
             int y = 58 + 70;
             this.addSlotToContainer(new Slot(playerInventory, row, x, y));
         }
@@ -41,22 +45,20 @@ public class ContainerGenerator extends Container implements IMachineStateContai
     public void detectAndSendChanges() {
         super.detectAndSendChanges();
 
-        /*if (te.getProgress() != te.getClientProgress() || te.getEnergy() != te.getClientEnergy()) {
+        if (te.getEnergy() != te.getClientEnergy()) {
             te.setClientEnergy(te.getEnergy());
-            te.setClientProgress(te.getProgress());
 
             for (IContainerListener listener : listeners) {
                 if (listener instanceof EntityPlayerMP) {
                     EntityPlayerMP player = (EntityPlayerMP) listener;
-                    int pct = 100 - te.getProgress() * 100 / GeneratorConfig.MAX_PROGRESS;
-                    Messages.INSTANCE.sendTo(new PacketSyncMachineState(te.getEnergy(), pct), player);
+                    Messages.INSTANCE.sendTo(new PacketSyncMachineState(te.getEnergy(), 0), player);
                 }
           }
-    }*/
+    }
     }
 
     @Override
     public void sync(int energy, int progress) {
-        //te.setClientEnergy(energy);
+        te.setClientEnergy(energy);
     }
 }
