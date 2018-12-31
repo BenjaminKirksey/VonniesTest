2a22c2c6aa8cff29286a765e28be44de56d56973
Jkosio23 <jkosio23@gmail.com>
2018-12-31 16:19:24 -0500

    Added Config options for Generator.

--- src/main/java/vonnie/vonniestest/config/GeneratorConfig.java
+++ src/main/java/vonnie/vonniestest/config/GeneratorConfig.java
@@ -8,4 +8,22 @@ public class GeneratorConfig {
 
     @Config.Comment(value = "Maximum amount of power for the generator")
     public static int MAX_POWER = 100000;
+
+    @Config.Comment(value = "Multiplier for Damage Generation")
+    public static float POWER_DAMAGE_FACTOR = 5.0f;
+
+    @Config.Comment(value = "Multiplier for Fishing Generation")
+    public static float POWER_FISHING_FACTOR = 7.0f;
+
+    @Config.Comment(value = "Event Tracking Box Length (Centered on Generator)[Odd]")
+    @Config.RangeInt(min = 3)
+    public static int TRACKING_BOX_LENGTH = 11;
+
+    @Config.Comment(value = "Event Tracking Box Width (Centered on Generator)[Odd]")
+    @Config.RangeInt(min = 3)
+    public static int TRACKING_BOX_WIDTH = 11;
+
+    @Config.Comment(value = "Event Tracking Box Height (Centered on Generator)[Odd]")
+    @Config.RangeInt(min = 3)
+    public static int TRACKING_BOX_HEIGHT = 7;
 }
